379f0123eb806abb956ec37b5628913d8fd1ce42
wyuuta <wilbudewa@yahoo.com>
2018-05-16 17:47:48 +0700

    add front controller

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use App\Wallet;
+use App\User;
+use Auth;
+use Redirect;
+
+class FrontController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showPrice()
+    {
+        $client = new Client();
+        $res = $client->request('GET', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,BTC,BCH,XRP,LTC,ETC,XLM,NEO,XEM,XVG&tsyms=IDR');
+        $prices = json_decode($res->getBody()));
+        $data['prices'] = $price['RAW'];
+        return view('index',$data);
+    }
+}
